57e2c34c887fe705ad5fb970a339ca3145f997c1
zamanrahimi <60590906+zamanrahimi@users.noreply.github.com>
2020-11-08 15:36:22 +0430

    test

--- gittest.php
+++
@@ -1,1 +0,0 @@
-This is a file added newly in Github or my local. 
--- new.php
+++
@@ -1,7 +0,0 @@
-This is a new file in github. 
-The second edit from github to Google. 
-
-This is from Google adalat test.
-
-this is what we want to be pushed from Google to Github. 
-test - from Gtihub to Google test jan khan. 
